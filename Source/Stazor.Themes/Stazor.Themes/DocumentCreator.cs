947e749fa137b7a998da540352fa81ec5ce500a8
finphie <19237170+finphie@users.noreply.github.com>
2021-07-22 10:16:05 +0900

    リファクタリング

---
+++ Source/Stazor.Themes/Stazor.Themes/DocumentCreator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Toolkit.HighPerformance.Helpers;
+using Stazor.Plugins;
+
+namespace Stazor.Themes
+{
+    /// <summary>
+    /// ドキュメントを並列で作成するための構造体です。
+    /// </summary>
+    readonly struct DocumentCreator : IAction
+    {
+        readonly IStazorDocument[] _documents;
+        readonly string[] _filePaths;
+
+        readonly INewDocumentsPlugin _newDocumentsPlugin;
+        readonly IEditDocumentPlugin[] _editDocumentPlugins;
+
+        /// <summary>
+        /// <see cref="DocumentCreator"/>構造体の新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="documents">ドキュメントの配列</param>
+        /// <param name="filePaths">ファイルパスの配列</param>
+        /// <param name="newDocumentsPlugin">ドキュメント新規作成用プラグイン</param>
+        /// <param name="editDocumentPlugins">ドキュメント編集用プラグインの配列</param>
+        public DocumentCreator(IStazorDocument[] documents, string[] filePaths, INewDocumentsPlugin newDocumentsPlugin, IEditDocumentPlugin[] editDocumentPlugins)
+        {
+            _documents = documents;
+            _filePaths = filePaths;
+
+            _newDocumentsPlugin = newDocumentsPlugin;
+            _editDocumentPlugins = editDocumentPlugins;
+        }
+
+        /// <inheritdoc/>
+        public void Invoke(int i)
+        {
+            var document = _newDocumentsPlugin.CreateDocument(_filePaths[i]);
+
+            foreach (var plugin in _editDocumentPlugins)
+            {
+                plugin.Execute(document);
+            }
+
+            _documents[i] = document;
+        }
+    }
+}
--- Source/Stazor.Themes/Stazor.Themes/Pipeline.cs
+++ Source/Stazor.Themes/Stazor.Themes/Pipeline.cs
@@ -36,22 +36,10 @@ namespace Stazor.Themes
             var documents = Document.CreateArray(filePaths.Length);
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
-            //foreach (var filePath in filePaths)
-            //{
-            //    var document = _newDocumentsPlugin.CreateDocument(filePath);
-
-            //    foreach (var plugin in _editDocumentPlugins)
-            //    {
-            //        plugin.Execute(document);
-            //    }
-
-            //    list.Add(document);
-            //}
-
             Microsoft.Toolkit.HighPerformance.Helpers.ParallelHelper.For(0, filePaths.Length, new DocumentCreator(documents, filePaths, _newDocumentsPlugin, _editDocumentPlugins));
 
             sw.Stop();
-            System.Console.WriteLine(sw.Elapsed);
+            Console.WriteLine(sw.Elapsed);
 
             foreach (var plugin in _postProcessingPlugins)
             {
@@ -66,36 +54,6 @@ namespace Stazor.Themes
             Unsafe.AsRef(_newDocumentsPlugin) = newDocumentsPlugin;
             Unsafe.AsRef(_editDocumentPlugins) = editDocumentPlugins;
             Unsafe.AsRef(_postProcessingPlugins) = postProcessingPlugins;
-        }
-    }
-
-    readonly struct DocumentCreator : Microsoft.Toolkit.HighPerformance.Helpers.IAction
-    {
-        readonly IStazorDocument[] _documents;
-        readonly string[] _filePaths;
-
-        readonly INewDocumentsPlugin _newDocumentsPlugin;
-        readonly IEditDocumentPlugin[] _editDocumentPlugins;
-
-        public DocumentCreator(IStazorDocument[] documents, string[] filePaths, INewDocumentsPlugin newDocumentsPlugin, IEditDocumentPlugin[] editDocumentPlugins)
-        {
-            _documents = documents;
-            _filePaths = filePaths;
-
-            _newDocumentsPlugin = newDocumentsPlugin;
-            _editDocumentPlugins = editDocumentPlugins;
-        }
-
-        public void Invoke(int i)
-        {
-            var document = _newDocumentsPlugin.CreateDocument(_filePaths[i]);
-
-            foreach (var plugin in _editDocumentPlugins)
-            {
-                plugin.Execute(document);
-            }
-
-            _documents[i] = document;
         }
     }
 }
