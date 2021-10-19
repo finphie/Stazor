90a8c6a520df9688d5c7b69d749354bb486c38a6
finphie <19237170+finphie@users.noreply.github.com>
2021-10-20 07:35:08 +0900

    JSON-LDを生成処理を、Utf8Jsonを利用せずに直接バッファに書き込むように変更

--- Source/Stazor.Plugins/Stazor.Plugins.Metadata.Breadcrumb/Breadcrumb.cs
+++ Source/Stazor.Plugins/Stazor.Plugins.Metadata.Breadcrumb/Breadcrumb.cs
@@ -1,8 +1,5 @@
-﻿using System.Runtime.Serialization;
-using Cysharp.Text;
+﻿using Cysharp.Text;
 using Stazor.Logging;
-using Utf8Json;
-using Utf8Json.Resolvers;
 using Utf8Utility;
 
 namespace Stazor.Plugins.Metadata;
@@ -36,20 +33,17 @@ public sealed class Breadcrumb : IEditDocumentPlugin
         ArgumentNullException.ThrowIfNull(document);
         _logger.Debug("Start");
 
-        using var builder = ZString.CreateUtf8StringBuilder(true);
+        using var builder = ZString.CreateUtf8StringBuilder();
 
+        // TODO: UTF-8にする。
         // TODO: "ホーム"文字列を可変
         builder.Append("<nav><ol class=\"breadcrumbs\"><li><a href=\"/\">ホーム</a><li><a href=\"");
-
-        var length = builder.Length;
-
         builder.Append(document.Metadata.Category);
         builder.Append("\">");
         builder.Append(document.Metadata.Category);
         builder.Append("</a>");
         builder.Append("<li>");
         builder.Append(document.Metadata.Title);
-
         builder.Append("</ol>");
         builder.Append("</nav>");
 
@@ -57,56 +51,23 @@ public sealed class Breadcrumb : IEditDocumentPlugin
 
         if (_settings.JsonLd)
         {
-            builder.Advance(-length);
+            // TODO: UTF-8にする。
+            const string Json1 = "{\"@context\":\"https://schema.org\",\"@type\":\"BreadcrumbList\",\"itemListElement\":" +
+                "[{\"@type\":\"ListItem\",\"position\":0,\"name\":\"ホーム\",\"item\":\"/\"}," +
+                "{\"@type\":\"ListItem\",\"position\":1,\"name\":\"";
+            const string Json2 = "\",\"item\":\"/";
+            const string Json3 = "\"}]}";
 
-            // TODO: JSON-LD 高速化
-            var json = new JsonLd
-            {
-                Context = "https://schema.org",
-                Type = "BreadcrumbList",
-                Items = new JsonLd.ItemListElement[2]
-            };
+            builder.Clear();
+            builder.Append(Json1);
+            builder.Append(document.Metadata.Category);
+            builder.Append(Json2);
+            builder.Append(document.Metadata.Category);
+            builder.Append(Json3);
 
-            for (var i = 0; i < json.Items.Length; i++)
-            {
-                json.Items[i] = new();
-                json.Items[i].Type = "ListItem";
-                json.Items[i].Position = i + 1;
-            }
-
-            json.Items[0].Name = "ホーム";
-            json.Items[1].Name = document.Metadata.Category!;
-            json.Items[0].Item = "/";
-            json.Items[1].Item = "/" + document.Metadata.Category;
-
-            var jsonLd = JsonSerializer.Serialize(json, StandardResolver.AllowPrivateExcludeNullSnakeCase);
-            document.Context.Add(_jsonLdKey, new Utf8Array(jsonLd));
+            document.Context.Add(_jsonLdKey, new Utf8Array(builder.AsSpan().ToArray()));
         }
 
         _logger.Debug("End");
-    }
-
-    sealed class JsonLd
-    {
-        [DataMember(Name = "@context")]
-        public string? Context { get; set; }
-
-        [DataMember(Name = "@type")]
-        public string? Type { get; set; }
-
-        [DataMember(Name = "itemListElement")]
-        public ItemListElement[]? Items { get; set; }
-
-        public sealed class ItemListElement
-        {
-            [DataMember(Name = "@type")]
-            public string? Type { get; set; }
-
-            public int Position { get; set; }
-
-            public string? Name { get; set; }
-
-            public string? Item { get; set; }
-        }
     }
 }
